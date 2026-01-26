69cec85b15ee567bb8de284c9af2a28788fd2f2a
Obiwumma Pascal <pobiwumma@gmail.com>
2026-01-26 22:14:24 +0100

    Add Suspense and lazy loading

--- new-portfolio-project/src/App.tsx
+++ new-portfolio-project/src/App.tsx
@@ -1,17 +1,23 @@
-import { useEffect } from 'react'; // Added useEffect import
-import React from 'react';
+import { useEffect, Suspense, lazy } from 'react'; 
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
-import About from './components/About.js';
-import Service from './components/Service.js';
-import Experience from './components/Experience.jsx';
-import Projects from './components/Projects.jsx';
-import Contact from './components/Contact.js';
-import Footer from './components/Footer.jsx';
+// import About from './components/About.js';
+// import Service from './components/Service.js';
+// import Experience from './components/Experience.jsx';
+// import Projects from './components/Projects.jsx';
+// import Contact from './components/Contact.js';
+// import Footer from './components/Footer.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css'
+
+const About = lazy(() => import('./components/About.js'));
+const Projects = lazy(() => import('./components/Projects.js'));
+const Contact = lazy(() => import('./components/Contact.js'));
+const Experience = lazy(() => import('./components/Experience.js'));
+const Service = lazy(() => import('./components/Service.js'));
+const Footer = lazy(() => import('./components/Footer.js'));
 
 function App() {
   useEffect(() => {
@@ -55,11 +61,12 @@ useEffect(() => {
 }, []);
 
   return (
-    <>
-      {/* Loader should be at the very top */}
+    <Suspense fallback = {
       <div id="loader" className="show">
         <div className="loader"></div>
       </div>
+    }>
+      {/* Loader should be at the very top */}
 
       <Navbar />
       <Hero />
@@ -72,7 +79,7 @@ useEffect(() => {
 
       {/* Back to top button */}
       <a href="#" className="btn back-to-top"><i className="fa fa-chevron-up"></i></a>
-    </>
+    </Suspense>
   );
 }
 
